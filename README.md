96319850ae05131d0707cd04dd0b7a5776b08b97
Juliana Carvalho <juliana.vcarvalhoo@gmail.com>
2022-05-04 15:46:25 -0300

    created

---
+++ README.md
@@ -0,0 +1,5 @@
+# Projeto-Estetica
+
+Projeto em desenvolvimento tanto para mobile quanto para desktop, foi utilizando para desenvolvimento do layout o figma também.
+
+La Romana Spa, é um projeto desenvolvido para uma empresa de SPA. 
---
+++ script.js
@@ -0,0 +1,32 @@
+/* ABRE E FECHA O MENU QUANDO CLICAR NO ICONE */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* QUANDO CLICAR EM UM ITEM DO MENU, ESCONDER O MENU */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* MUDA O HEADER DA PAGINA QUANDO DER SCROLL*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    /* scroll é maior que a altura do header*/
+    header.classList.add('scroll')
+  } else {
+    /* scroll é menor que a altura do header*/
+    header.classList.remove('scroll')
+  }
+})
